3201678974ca9b5c56a8942f2c526194c284f79d
LionelMv <mvangicode@gmail.com>
2024-11-11 14:09:33 +0300

    feat: Change home the home and about url patterns

--- SI_RC/calculator/urls.py
+++ SI_RC/calculator/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('calculator/', views.Indice, name='calculator-home'),
-  path('calculator/about/', views.about, name='calculator-about')
+  path('', views.Indice, name='calculator-home'),
+  path('about/', views.about, name='calculator-about')
 ]
